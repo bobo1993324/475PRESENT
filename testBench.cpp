8dfbc5222a3f0cd91c74e4872a004b0f4d0591b2
Boren Zhang <bobo1993324@gmail.com>
2014-04-04 16:20:05 -0400

    fix testBench

--- testBench.cpp
+++ testBench.cpp
@@ -7,36 +7,23 @@ using namespace std;
 
 int main() {
     uberzahl key;
-    uberzahl key2("1208925819614629174706175"); // 0xFFFFFFFFFFFFFFFFFFFF
+    uberzahl key2("18446744073709551615"); // 0xFFFFFFFFFFFFFFFFFFFF
     uberzahl plaintext;
-    uberzahl plaintext2("1208925819614629174706175"); // 0xFFFFFFFFFFFFFFFFFFFF
+    uberzahl plaintext2("18446744073709551615"); // 0xFFFFFFFFFFFFFFFFFFFF
 
     Present p(key, 10);
     uberzahl c = p.encrypt(plaintext);
-    cout << "PlainText: 0; Key: 0; CipherText: " << c << endl;
+    cout << "PlainText: 0; Key: 0; CipherText: " << c << "; Decrypted Back: " << p.decrypt(c) <<  endl;
 
     Present p2(key2, 10);
     c = p2.encrypt(plaintext);
-    cout << "PlainText: 0; Key: FFFFFFFFFFFFFFFFFFFF; CipherText: " << c << endl;
+    cout << "PlainText: 0; Key: 18446744073709551615; CipherText: " << c << "; Decrypted Back: " << p2.decrypt(c) << endl;
 
     Present p3(key, 10);
     c = p3.encrypt(plaintext2);
-    cout << "PlainText: FFFFFFFFFFFFFFFFFFFF; Key: 0; CipherText: " << c << endl;
+    cout << "PlainText: 18446744073709551615; Key: 0; CipherText: " << c << "; Decrypted Back: " << p3.decrypt(c) <<endl;
 
     Present p4(key2, 10);
     c = p4.encrypt(plaintext2);
-    cout << "PlainText: FFFFFFFFFFFFFFFFFFFF; Key: FFFFFFFFFFFFFFFFFFFF; CipherText: " << c << endl;
-
-    vector <uberzahl> keys, plaintexts, cyphers;
-    keys.push_back(key);
-    keys.push_back(key2);
-    keys.push_back(key);
-    keys.push_back(key2);
-    plaintexts.push_back(plaintext);
-    plaintexts.push_back(plaintext);
-    plaintexts.push_back(plaintext2);
-    plaintexts.push_back(plaintext2);
-    cyphers = encrypt(keys, plaintexts);
-    cout << "cyphers: " << " " << cyphers[0] << " " << cyphers[1] << " " << cyphers[2] << " " << cyphers[3] << endl;
-
+    cout << "PlainText: 18446744073709551615; Key: 18446744073709551615; CipherText: " << c << "; Decrypted Back: " << p4.decrypt(c) << endl;
 }
